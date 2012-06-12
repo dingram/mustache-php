4cba1cb4316185b6e481ae37808739c0aed674aa
Dave Ingram <dave@dmi.me.uk>
2012-06-12 10:35:55 +0100

    Added further tests

--- MustacheTest.php
+++ MustacheTest.php
@@ -4,27 +4,62 @@ require 'MustacheTemplate.php';
 
 $tpl = MustacheTemplate::fromString('This is a {{test}}');
 
+var_dump($tpl->__toString());
 var_dump($tpl->render(array()));
 var_dump($tpl->render(array('test'=>'test')));
 var_dump($tpl->render(array('test'=>'test with <b>html</b>')));
+
+echo str_repeat('-', 78)."\n";
 
 $tpl = MustacheTemplate::fromString('This is a {{{test}}}');
 
+var_dump($tpl->__toString());
 var_dump($tpl->render(array()));
 var_dump($tpl->render(array('test'=>'test')));
 var_dump($tpl->render(array('test'=>'test with <b>html</b>')));
+
+echo str_repeat('-', 78)."\n";
 
 $tpl = MustacheTemplate::fromString('This is a {{& test}}');
 
+var_dump($tpl->__toString());
 var_dump($tpl->render(array()));
 var_dump($tpl->render(array('test'=>'test')));
 var_dump($tpl->render(array('test'=>'test with <b>html</b>')));
 
+echo str_repeat('-', 78)."\n";
+
 $tpl = MustacheTemplate::fromString('{{test}}This is a {{test}}');
 
+var_dump($tpl->__toString());
 var_dump($tpl->render(array()));
 var_dump($tpl->render(array('test'=>'test')));
 
+echo str_repeat('-', 78)."\n";
+
 $tpl = MustacheTemplate::fromString('{{test}}This is a {{&test}}{{test}}');
 
+var_dump($tpl->__toString());
+var_dump($tpl->render(array('test'=>'test')));
+
+echo str_repeat('-', 78)."\n";
+
+$tpl = MustacheTemplate::fromString('{{test}}{{foo}}This is a {{&foo}}{{&test}}{{test}}{{{foo}}}');
+
+var_dump($tpl->__toString());
+var_dump($tpl->render(array('test'=>'test')));
+
+echo str_repeat('-', 78)."\n";
+
+$tpl = MustacheTemplate::fromString('Test {{>test}} partially {{> partial}}');
+
+var_dump($tpl->__toString());
+var_dump($tpl->render(array('test'=>'test')));
+
+echo str_repeat('-', 78)."\n";
+
+$tpl = MustacheTemplate::fromString('Test {{#test}}passed{{/test}}{{^test}}failed{{/test}}.');
+
+var_dump($tpl->__toString());
+var_dump($tpl->render(array()));
 var_dump($tpl->render(array('test'=>'test')));
