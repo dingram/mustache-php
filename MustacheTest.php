96166cc124aa85f0eb71c56ade1a555d81042355
Dave Ingram <dmi@teamgroupspaces.com>
2012-06-12 11:49:45 +0100

    Further tests for section functionality

--- MustacheTest.php
+++ MustacheTest.php
@@ -58,8 +58,36 @@ var_dump($tpl->render(array('test'=>'test')));
 
 echo str_repeat('-', 78)."\n";
 
-$tpl = MustacheTemplate::fromString('Test {{#test}}passed{{/test}}{{^test}}failed{{/test}}.');
+$tpl = MustacheTemplate::fromString('Test {{#test}}non-{{.}}-empty{{/test}}{{^test}}empty{{/test}}.');
 
 var_dump($tpl->__toString());
 var_dump($tpl->render(array()));
 var_dump($tpl->render(array('test'=>'test')));
+
+echo str_repeat('-', 78)."\n";
+
+$tpl = MustacheTemplate::fromString('Test{{#foo}} foo{{#bar}} bar {{/bar}}foo{{/foo}}.{{#bar}} Outer bar{{/bar}}');
+
+var_dump($tpl->__toString());
+var_dump($tpl->render(array()));
+var_dump($tpl->render(array('foo'=>true)));
+var_dump($tpl->render(array('bar'=>true)));
+var_dump($tpl->render(array('foo'=>true, 'bar'=>true)));
+var_dump($tpl->render(array('foo'=>array('bar'=>true))));
+
+echo str_repeat('-', 78)."\n";
+
+$tpl = MustacheTemplate::fromString('start:{{#person}} [ name: {{name}} ]{{/person}}');
+
+var_dump($tpl->__toString());
+var_dump($tpl->render(array()));
+var_dump($tpl->render(array('person'=>array(
+	array('name' => 'Alice'),
+))));
+var_dump($tpl->render(array('person'=>array(
+	array('name' => 'Alice'),
+	array('name' => 'Bob'),
+	array('name' => 'Carol'),
+	array('name' => 'Derek'),
+	array('name' => 'Edna'),
+))));
